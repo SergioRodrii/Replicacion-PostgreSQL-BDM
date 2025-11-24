3a855d7c7472cc6eabf1036836db6fbd4db0f7f5
SergioRodrii <serocastro@gmail.com>
2025-11-23 19:29:44 -0500

    Ajustes en las tablas de particionamiento -Index

--- schema.sql
+++ schema.sql
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS estudiante (
     estu_nacionalidad VARCHAR(80),
     estu_privado_libertad BOOLEAN,
     estu_estadoinvestigacion VARCHAR(50)
-);
+)PARTITION BY HASH (estu_consecutivo); 
 
 -- TABLA UBICACION_PRESENTACION
 CREATE TABLE IF NOT EXISTS ubicacion_presentacion (
@@ -61,11 +61,12 @@ CREATE TABLE IF NOT EXISTS ubicacion_presentacion (
 
 -- TABLA UBICACION_RESIDENCIA
 CREATE TABLE IF NOT EXISTS ubicacion_residencia (
-    id_ubicacion_residencia SERIAL PRIMARY KEY,
+    id_ubicacion_residencia SERIAL,
     estu_consecutivo VARCHAR(255) REFERENCES estudiante(estu_consecutivo),
     cod_mcpio BIGINT REFERENCES municipio(cod_mcpio),
-    estu_pais_reside VARCHAR(130)
-);
+    estu_pais_reside VARCHAR(130),
+    PRIMARY KEY (id_ubicacion_residencia, cod_mcpio)
+) PARTITION BY LIST (cod_mcpio);
 
 -- TABLA FAMILIA
 CREATE TABLE IF NOT EXISTS familia (
@@ -84,7 +85,7 @@ CREATE TABLE IF NOT EXISTS familia (
 
 -- TABLA RESULTADOS
 CREATE TABLE IF NOT EXISTS resultados (
-    id_resultado SERIAL PRIMARY KEY,
+    id_resultado SERIAL,
     estu_consecutivo VARCHAR(255) REFERENCES estudiante(estu_consecutivo),
     periodo INT REFERENCES periodo(periodo),
     desemp_ingles VARCHAR(10),
@@ -93,8 +94,9 @@ CREATE TABLE IF NOT EXISTS resultados (
     punt_sociales_ciudadanas INT,
     punt_c_naturales INT,
     punt_lectura_critica INT,
-    punt_global INT
-);
+    punt_global INT,
+    PRIMARY KEY (id_resultado, periodo)
+) PARTITION BY RANGE (periodo);
 
 -- TABLA ERRORES_CARGA
 CREATE TABLE IF NOT EXISTS errores_carga (
@@ -103,3 +105,80 @@ CREATE TABLE IF NOT EXISTS errores_carga (
     json_data TEXT,
     fecha_error TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+/*
+======================================================================
+PARTICIONAMIENTO DE LA TABLA ESTUDIANTE
+======================================================================
+*/
+
+CREATE TABLE estudiante_p0 PARTITION OF estudiante
+FOR VALUES WITH (MODULUS 4, REMAINDER 0);
+
+CREATE TABLE estudiante_p1 PARTITION OF estudiante
+FOR VALUES WITH (MODULUS 4, REMAINDER 1);
+
+CREATE TABLE estudiante_p2 PARTITION OF estudiante
+FOR VALUES WITH (MODULUS 4, REMAINDER 2);
+
+CREATE TABLE estudiante_p3 PARTITION OF estudiante
+FOR VALUES WITH (MODULUS 4, REMAINDER 3);
+
+
+CREATE INDEX estu_fecha_p0 ON estudiante_p0 (estu_fechanacimiento);
+CREATE INDEX estu_fecha_p1 ON estudiante_p1 (estu_fechanacimiento);
+CREATE INDEX estu_fecha_p2 ON estudiante_p2 (estu_fechanacimiento);
+CREATE INDEX estu_fecha_p3 ON estudiante_p3 (estu_fechanacimiento);
+
+/*
+======================================================================
+
+======================================================================
+*/
+
+
+
+/*
+======================================================================
+PARTICIONAMIENTO DE LA TABLA UBICACION_RESIDENCIA
+======================================================================
+*/
+
+CREATE TABLE ubicacion_residencia_medellin
+    PARTITION OF ubicacion_residencia
+    FOR VALUES IN (5001);
+
+CREATE TABLE ubicacion_residencia_bogota
+    PARTITION OF ubicacion_residencia
+    FOR VALUES IN (11001);
+
+CREATE TABLE ubicacion_residencia_default
+    PARTITION OF ubicacion_residencia
+    DEFAULT;
+
+/*
+======================================================================
+
+======================================================================
+*/
+
+
+
+/*
+======================================================================
+PARTICIONAMIENTO DE LA TABLA 
+======================================================================
+*/
+
+CREATE TABLE resultados_2020 PARTITION OF resultados FOR VALUES FROM (20201) TO (20202); 
+CREATE TABLE resultados_2021 PARTITION OF resultados FOR VALUES FROM (20211) TO (20212); 
+CREATE TABLE resultados_2022 PARTITION OF resultados FOR VALUES FROM (20221) TO (20222); 
+CREATE TABLE resultados_default PARTITION OF resultados DEFAULT;
+
+/*
+======================================================================
+
+======================================================================
+*/
